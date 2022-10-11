d3ed9cc7a7e354f2e62b597f20f25aba01d125b3
shifanazzz <110272583+shifanazzz@users.noreply.github.com>
2022-10-11 20:31:41 +0530

    Add files via upload

---
+++ ShifaNaz_FOPJava_LabAlgo2/src/org/greatlearning/transaction/services/NotesCount.java
@@ -0,0 +1,34 @@
+package org.greatlearning.transaction.services;
+
+public class NotesCount {
+	public void notesCount(int notes[], int amount) {
+		int a[] = new int[notes.length];
+		if (amount == 0) {
+			System.out.println("Amount entered is 0. Try again!");
+			return;
+		}
+		try {
+			for (int i = 0; i < notes.length; i++) {
+				if (amount >= notes[i]) {
+					a[i] = amount / notes[i];
+					amount = amount - a[i] * notes[i];
+				}
+
+			}
+			if (amount > 0)
+				System.out.println("Exact amount cant be given with the highest denomination");
+			else {
+				System.out.println("Your payment approach in order to give min number of notes will be");
+				for (int i = 0; i < notes.length; i++) {
+					if (a[i] != 0) {
+						System.out.println(notes[i] + " : " + a[i]);
+					}
+				}
+			}
+		} catch (ArithmeticException e) {
+			System.out.println(e + "Notes of denominations 0 is invalid");
+
+		}
+	}
+
+}
